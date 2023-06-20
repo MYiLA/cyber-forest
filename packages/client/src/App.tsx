eebfde9b30c4d4cbc1bfcf2fb124ff452c1fa75f
AndreyS <suvorov.av93@gmail.com>
2023-06-21 02:50:43 +0300

    fix: lint

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,24 @@
 import './App.css'
 import { BrowserRouter } from 'react-router-dom'
 import { MainRouter } from './core/router/main-router'
-import { useSelector } from 'react-redux'
-import { TRootState } from './core/store/store'
+import { Provider, useSelector } from 'react-redux'
+import store, { TRootState } from './core/store/store'
 import React, { useEffect } from 'react'
 import { TestNav } from './components/test-nav/test-nav'
 
 function App() {
-  const { themeName } = useSelector((store: TRootState) => store.theme)
-  useEffect(() => {
-    document.body.className = `body-${themeName}`
-  }, [themeName])
+  // const { themeName } = useSelector((store: TRootState) => store.theme)
+  // useEffect(() => {
+  //   document.body.className = `body-${themeName}`
+  // }, [themeName])
 
   return (
-    <BrowserRouter>
-      <TestNav />
-      <MainRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <TestNav />
+        <MainRouter />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import store from './core/store/store'
-import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 )
