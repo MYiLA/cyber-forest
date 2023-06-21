5be1c889fd760faae57697a8c9187432570ac2ed
AndreyS <suvorov.av93@gmail.com>
2023-06-21 16:02:50 +0300

    fix: jest & act()

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,14 +1,16 @@
 import App from './App'
 import { render, screen } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 
 const appContent = 'HOME'
 
-// @ts-ignore
-global.fetch = jest.fn(() =>
-  Promise.resolve({ json: () => Promise.resolve('hey') })
-)
+// global.fetch = jest.fn(() =>
+//   Promise.resolve({ json: () => Promise.resolve('hey') })
+// )
 
 test('Example test', async () => {
-  render(<App />)
+  await act(() => {
+    render(<App />)
+  })
   expect(screen.getByText(appContent)).toBeDefined()
 })
