4f865dd3a61176965f9cedf41a5385d55a4c8fac
AndreyS <suvorov.av93@gmail.com>
2023-06-21 07:44:14 +0300

    feat: chat & game reducers

---
+++ packages/client/src/core/store/reducers/chat-reducer.ts
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {}
+
+export const chatSlice = createSlice({
+  name: 'chat',
+  initialState,
+  reducers: {},
+  // eslint-disable-next-line
+  extraReducers: builder => {},
+})
+
+// eslint-disable-next-line
+export const {} = chatSlice.actions
+export default chatSlice.reducer
--- packages/client/src/core/store/root-reducer.ts
+++ packages/client/src/core/store/root-reducer.ts
@@ -1,6 +1,8 @@
 import user from './reducers/user-reducer'
 import players from './reducers/players-reducer'
 import theme from './reducers/theme-reducer'
+import game from './reducers/game-reducer'
+import chat from './reducers/chat-reducer'
 
-const rootReducer = { user, players, theme }
+const rootReducer = { user, players, theme, game, chat }
 export default rootReducer
