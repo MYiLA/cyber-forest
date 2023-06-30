12ae533109b3b88f78e7bb6d6c9c0988674a1861
AndreyS <suvorov.av93@gmail.com>
2023-06-30 23:31:55 +0300

    feature/cyb-40-настроить-express-для-server-side-rendering

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,37 @@
-import dotenv from 'dotenv'
-import cors from 'cors'
-dotenv.config()
-
+import fs from 'node:fs/promises'
 import express from 'express'
-import { createClientAndConnect } from './db'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+const PORT = process.env.PORT || 3001
+
+const clientDir = path.join(__dirname, '..', '..', 'client', 'dist')
+
+const templateDemo = await fs.readFile(`${clientDir}/index-ssr.html`, 'utf-8')
+const templateMain = await fs.readFile(`${clientDir}/index.html`, 'utf-8')
 
 const app = express()
-app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
 
-createClientAndConnect()
+app.use('/assets', express.static(path.join(clientDir, 'assets')))
+app.get('/', async (_, res) => {
+  try {
+    // @ts-ignore
+    const render = (await import('./render.js')).render
+    const rendered = await render()
+    const html = templateDemo.replace(`<!--app-html-->`, rendered.html ?? '')
 
-app.get('/', (_, res) => {
-  res.json('👋 Howdy from the server :)')
+    res.send(html)
+  } catch (e: unknown) {
+    res.status(500).end((e as Error).stack)
+  }
 })
 
-app.listen(port, () => {
-  console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+app.get('*', (_, res) => {
+  res.send(templateMain)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server started at http://localhost:${PORT}`)
 })
