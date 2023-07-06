e7ea00b6b0bea6925d62e95f297293af5c808079
AndreyS <suvorov.av93@gmail.com>
2023-07-06 18:19:52 +0300

    [CYB-63] Серверная инфраструктура для форума

--- packages/server/db.ts
+++ packages/server/db.ts
@@ -1,13 +1,18 @@
 import { Client } from 'pg'
 
-const { POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB, POSTGRES_PORT } =
-  process.env
+const {
+  POSTGRES_USER,
+  POSTGRES_HOST,
+  POSTGRES_PASSWORD,
+  POSTGRES_DB,
+  POSTGRES_PORT,
+} = process.env
 
 export const createClientAndConnect = async (): Promise<Client | null> => {
   try {
     const client = new Client({
       user: POSTGRES_USER,
-      host: 'localhost',
+      host: POSTGRES_HOST,
       database: POSTGRES_DB,
       password: POSTGRES_PASSWORD,
       port: Number(POSTGRES_PORT),
