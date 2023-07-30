cf2ff8666866809bec0a34514694fe997787f0aa
Ruslan Ivanov <ruslan4432013@gmail.com>
2023-07-30 14:00:54 +0300

    feature: add leaderboard api

--- packages/client/src/core/store/store.ts
+++ packages/client/src/core/store/store.ts
@@ -17,3 +17,8 @@ export const createStore = (initialState: Record<string, unknown>) => {
 
 export type RootState = ReturnType<typeof store.getState>;
 export type Dispatch = typeof store.dispatch;
+
+declare global {
+  type RootState = ReturnType<typeof store.getState>;
+  type AppDispatch = typeof store.dispatch;
+}
